b127166699b247e1d6fd58bb4c42fc9de67acf65
Christina Bowers <christinabowers1@gmail.com>
2020-11-10 16:26:55 -0600

    Task manager

--- db/migrate/20201110184421_create_tasks.rb
+++ db/migrate/20201110184421_create_tasks.rb
@@ -3,8 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.boolean :completed
-      t.false :default
+      t.boolean :completed, :false
 
       t.timestamps
     end
